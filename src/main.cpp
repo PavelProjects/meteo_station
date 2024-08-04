c57e0e0b17d3605db3b1fd3102b82d69949458e5
PavelProjects <paveloalexeev52@gmail.com>
2024-08-04 17:09:24 +0300

    sensors rework

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,17 @@
 #include <SmartThing.h>
 // Библиотека для обновления по беспроводной сети
 #include <ArduinoOTA.h>
-// Библиотека для работы с метеосенсором
-#include <GyverBME280.h>
+// Библиотека для работы с датчиком давления
+#include <Adafruit_BMP280.h>
+// Библиотека для работы с датчиком температуры и влажности
+#include <AHT10.h>
 
 void addSensors();
 
-// Метесенсор
-GyverBME280 bme;
-
-void test() {
-
-  SmartThing.addAnalogSensor("temp", 1);
-  SmartThing.addDigitalSensor("button", 2);
-  SmartThing.addSensor("pressure", []() {
-    return bme.readPressure() * 0.00750062;
-  });
-
-  SmartThing.addDeviceState("wifi", []() {
-    return SmartThing.wifiConnected() ? 
-      "connected" : "disconnected";
-  });
-}
+// Датчик давления
+Adafruit_BMP280 bmp;
+// Датчик температуры и влажности
+AHT10 aht(AHT10_ADDRESS_0X38, AHT20_SENSOR);
 
 // Настройка перед запуском
 void setup() {
@@ -43,10 +33,24 @@ void setup() {
     ArduinoOTA.begin();
   }
 
-  // Запуск метесенсора
-  if (!bme.begin(0x76)) {
-    LOGGER.error("main", "Bme initialization error!");
+  // Запуск датчиков
+  if (bmp.begin()) {
+    bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+    LOGGER.info("main", "Bmp sensor initialization finished!");
+  } else {
+    LOGGER.error("main", "Bmp sensor initialization error!");
   }
+
+  if (aht.begin()) {
+    LOGGER.info("main", "Aht sensor initialization finished!");
+  } else {
+    LOGGER.error("main", "Aht sensor initialization error!");
+  }
+
   LOGGER.info("main", "Setup finished");
 }
 
@@ -61,16 +65,16 @@ void loop() {
 
 void addSensors() {
   // Добавление различных сенсоров при помощи разработанной библиотеки
-  // Показатель температуры
+  // // Показатель температуры
   SmartThing.addSensor("temperature", []() {
-    return bme.readTemperature();
+    return aht.readTemperature();
   });
-  // Показатель влажности
+  // // Показатель влажности
   SmartThing.addSensor("humidity", []() {
-    return bme.readHumidity();
+    return aht.readHumidity();
   });
   // Показатель давления
   SmartThing.addSensor("pressure", []() {
-    return bme.readPressure() * 0.00750062;
+    return bmp.readPressure() * 0.00750062;
   });
 }
